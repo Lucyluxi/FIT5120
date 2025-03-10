880815aa98b2842c18040646fbf09ae6cb6d4d9b
Xi Lu <lucyluxi2001@gmail.com>
2025-03-11 09:33:32 +1100

    Initialization

---
+++ src/components/BHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- Using Bootstrap's Header template (starter code) -->
+  <!-- https://getbootstrap.com/docs/5.0/examples/headers/ -->
+  <div class="container">
+    <header
+      class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom"
+    >
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap">
+            <use xlink:href="#bootstrap"></use>
+          </svg>
+        </a>
+      </div>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><router-link to="/" class="nav-link px-2 link-secondary">Home</router-link></li>
+        <li><router-link to="/" class="nav-link px-2">Features</router-link></li>
+        <li><router-link to="/" class="nav-link px-2">Pricing</router-link></li>
+        <li><router-link to="/" class="nav-link px-2">FAQs</router-link></li>
+        <li><router-link to="/about" class="nav-link px-2">About</router-link></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        <button type="button" class="btn btn-primary">Sign-up</button>
+      </div>
+    </header>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="row">
+    <!-- <div class="col-12 d-flex justify-content-center mt-5">
+      <img :src="imageUrl1" alt="Mental Health Header" class="mx-auto responsive-image" />
+    </div> -->
+    <div
+      class="brief column mt-3 text-center"
+      style="display: flex; flex-direction: column; align-items: center; background-color: white"
+    >
+      <h1 class="col-lg-8 col-10 mt-5">Welcome to Home page</h1>
+      <span class="col-lg-8 col-10 my-3" style="font-size: 1.25rem"> testing </span>
+      <p class="col-lg-8 col-10 my-5" style="font-size: 1.25rem">testing</p>
+    </div>
+  </div>
+</template>
+
+<!-- <script>
+import imageUrl from '@/assets/images/mentalHealth.jpg'
+import imageUrl1 from '@/assets/images/homeHeader3.png'
+
+export default {
+  data() {
+    return {
+      imageUrl,
+      imageUrl1,
+    }
+  },
+  methods: {
+    goToBooking() {
+      this.$router.push('/booking')
+    },
+    goToResource() {
+      this.$router.push('/resourceView')
+    },
+    goToParent() {
+      this.$router.push('/forParentView')
+    },
+    goToAbout() {
+      this.$router.push('/contact')
+    },
+  },
+}
+</script> -->
+
+<style scoped>
+.responsive-image {
+  width: 40%;
+  height: auto;
+}
+
+@media (max-width: 992px) {
+  .responsive-image {
+    width: 60%;
+  }
+}
+
+@media (max-width: 768px) {
+  .responsive-image {
+    width: 80%;
+  }
+}
+
+@media (max-width: 576px) {
+  .responsive-image {
+    width: 100%;
+  }
+}
+
+h2 {
+  font-size: 2rem;
+  color: #007bff; /* Primary color */
+  margin-top: 20px;
+}
+
+.btn {
+  padding: 10px 20px;
+  font-size: 1rem;
+}
+</style>
